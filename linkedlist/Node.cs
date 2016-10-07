4189e4384d863319870a75790ce4d71ed8599905
alunosnet <alunosnet@gmail.com>
2016-10-07 11:25:38 +0100

    Project started

---
+++ linkedlist/Node.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace linkedlist {
+    public class Node {
+        public int data;
+        public Node next;
+
+        public Node(int data) {
+            this.data = data;
+            next = null;
+        }
+
+        public void Print() {
+            Console.Write($"{data}|->");
+            if (next != null) {
+                next.Print();
+
+            }
+           
+        }
+        public void AddSorted(int data) {
+            if (next == null)
+                next = new Node(data);
+            else if(data<next.data) {
+                Node temp = new Node(data);
+                temp.next = this.next;
+                this.next = temp;
+            }else {
+                next.AddSorted(data);
+            }
+        }
+        public void AddAtEnd(int data) {
+            if (next == null) {
+                next = new Node(data);
+                return;
+            }
+            next.AddAtEnd(data);
+        }
+    }
+
+    public class MyLinkedList {
+        public Node head;
+
+        public MyLinkedList() {
+            head = null;
+        }
+
+
+        public void AddAtEnd(int data) {
+            if (head == null)
+                head = new Node(data);
+            else
+                head.AddAtEnd(data);
+        }
+
+        public void AddToBeginning(int data) {
+            if (head == null) {
+                head = new Node(data);
+            }else {
+                Node temp = new Node(data);
+                temp.next = head;
+                head = temp;
+            }
+        }
+
+        public void AddSorted(int data) {
+            if (head == null) {
+                head = new Node(data);
+            }else {
+                if (data < head.data) {
+                    AddToBeginning(data);
+                } else {
+                    head.AddSorted(data);
+                }
+            }
+        }
+
+        public void Remove(int data) {
+            Node temp = head;
+            if (head.data == data) {
+                head = head.next;
+                return;
+            }
+            while (temp != null) {
+                if (temp.data == data) {
+                    Node prev = head;
+                    while (prev.next != temp)
+                        prev = prev.next;
+                    prev.next = temp.next;
+                }
+                temp = temp.next;
+            }
+        }
+        public void Print() {
+            head.Print();
+        }
+    }
+}
--- linkedlist/Program.cs
+++ linkedlist/Program.cs
@@ -7,6 +7,28 @@ using System.Threading.Tasks;
 namespace linkedlist {
     class Program {
         static void Main(string[] args) {
+            //Node node = new Node(5);
+            //node.next = new Node(10);
+            //node.next.next = new Node(1);
+            //node.AddAtEnd(10);
+            //node.AddAtEnd(11);
+            //node.AddAtEnd(1);
+            //node.Print();
+            //MyLinkedList myList = new MyLinkedList();
+            //myList.AddAtEnd(5);
+            //myList.AddAtEnd(10);
+            //myList.AddAtEnd(1);
+            MyLinkedList myList = new MyLinkedList();
+            //myList.AddToBeginning(5);
+            //myList.AddToBeginning(10);
+            //myList.AddToBeginning(1);
+            myList.AddSorted(5);
+            myList.AddSorted(10);
+            myList.AddSorted(1);
+            myList.Print();
+            myList.Remove(10);
+            myList.Print();
+            Console.ReadKey();
         }
     }
 }
